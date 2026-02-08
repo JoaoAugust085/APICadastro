2dbd0085ded53d008a286da60faa730c43a592bb
Joao Augusto <joaoag108@gmail.com>
2026-02-08 01:40:18 -0300

    Criados Controller e Model para Funcionario, e entidade BD

--- src/main/java/com/jaugusto/APICadastro/FuncionarioModel.java
+++ src/main/java/com/jaugusto/APICadastro/FuncionarioModel.java
@@ -1,4 +1,49 @@
 package com.jaugusto.APICadastro;
 
+
+import jakarta.persistence.*;
+
+@Entity
+@Table(name = "tb_cadastro")
 public class FuncionarioModel {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    Long id;
+    String nome;
+    String email;
+    int idade;
+
+    public FuncionarioModel(String nome, String email, int idade) {
+        this.nome = nome;
+        this.email = email;
+        this.idade = idade;
+    }
+
+    public FuncionarioModel() {
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public int getIdade() {
+        return idade;
+    }
+
+    public void setIdade(int idade) {
+        this.idade = idade;
+    }
 }
