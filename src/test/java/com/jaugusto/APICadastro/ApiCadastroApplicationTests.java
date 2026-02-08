98f9b3cb7e2c28b5d54e3ea35b948cabc0d554f3
Joao Augusto <joaoag108@gmail.com>
2026-02-07 23:31:56 -0300

    Primeiro commit

---
+++ src/test/java/com/jaugusto/APICadastro/ApiCadastroApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jaugusto.APICadastro;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiCadastroApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
