58ed38b8d98c17d78f5ff5338923a374e78881a6
Joao Augusto <joaoag108@gmail.com>
2026-02-10 18:04:11 -0300

    Alteração da classe Model e criação da Interface Repository para comportar conexão com BD

--- src/main/java/com/jaugusto/APICadastro/Funcionarios/FuncionarioModel.java
+++ src/main/java/com/jaugusto/APICadastro/Funcionarios/FuncionarioModel.java
@@ -3,15 +3,22 @@ package com.jaugusto.APICadastro.Funcionarios;
 
 import com.jaugusto.APICadastro.Atribuicoes.AtribuicoesModel;
 import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
 
 @Entity
 @Table(name = "tb_funcionario")
+@NoArgsConstructor
+@AllArgsConstructor
+@Data
 public class FuncionarioModel {
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
     private String nome;
+    @Column(unique = true)
     private String email;
     private int idade;
     //Vários Funcionários podem tem uma atribuição
@@ -19,36 +26,5 @@ public class FuncionarioModel {
     @JoinColumn(name = "atribuicoes_id")  //Chave estrangeira.
     private AtribuicoesModel atribuicoes;
 
-    public FuncionarioModel(String nome, String email, int idade) {
-        this.nome = nome;
-        this.email = email;
-        this.idade = idade;
-    }
 
-    public FuncionarioModel() {
-    }
-
-    public String getNome() {
-        return nome;
-    }
-
-    public void setNome(String nome) {
-        this.nome = nome;
-    }
-
-    public String getEmail() {
-        return email;
-    }
-
-    public void setEmail(String email) {
-        this.email = email;
-    }
-
-    public int getIdade() {
-        return idade;
-    }
-
-    public void setIdade(int idade) {
-        this.idade = idade;
-    }
 }
---
+++ src/main/java/com/jaugusto/APICadastro/Funcionarios/FuncionarioRepository.java
@@ -0,0 +1,6 @@
+package com.jaugusto.APICadastro.Funcionarios;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface FuncionarioRepository extends JpaRepository<FuncionarioModel, Long> {
+}
